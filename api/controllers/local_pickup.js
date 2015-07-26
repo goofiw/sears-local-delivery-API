f313a7de4ca30d466de6792e758d0d11c81064b7
goofiw <goofiw@gmail.com>
2015-07-26 00:54:11 -0700

    products now returns an array of both the product and stores

--- api/controllers/local_pickup.js
+++ api/controllers/local_pickup.js
@@ -1,29 +1,29 @@
 var jsonQuery = require('json-query');
 var Promise = require('bluebird');
+var join = Promise.join;
 var request = Promise.promisify(require('request'));
 var secrets = require('../../secrets');
 
 module.exports = {
   getProducts: getProducts
 }
-function getLocalProduct(partNumber, zip, cb) {
+
+
+function getLocalProduct(partNumber, zip) {
   var localBody,
       storeInfo,
       url,
       radius = 50;
        url = 'http://api.developer.sears.com/v2.1/product/availability/pickup//Sears/json/' + partNumber + '/' + zip + '?maxDistance=' + radius + '&quantity=' + 1 + '&storeIndex=10&apikey=' + secrets.searsKey;
-  request(url, function (error, response, localBody) {
+  return request(url).spread(function(response, localBody) {
   var storeInfo;
-    if (error) {
-      res.send(error);
-    } else {
       localBody = JSON.parse(localBody);
       storeInfo = localBody.RefactorSPUPickupData.SPUStores; 
       if (storeInfo[0] && storeInfo[0].SPUAvailabilityMsg === "In Stock Today") {
-        return cb(storeInfo);
+        console.log('STORE INFO   \n\n\n');
+        return storeInfo;
       }
-    }
-  });
+  }).catch(function(e) {console.log(e);});
 }
 
 function getNearestStore(zip){
@@ -45,40 +45,24 @@ function getProducts(req, res){
       cb,
       nearestStore,
       latlong = req.swagger.params.latlong.value;
-  request("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + latlong + "&sensor=false", function(err, response, locationBody){
-    if (err) {
-      res.send(err)
-    }
-    locationBody = JSON.parse(locationBody);
-    zip = jsonQuery('results[types=postal_code].address_components', {data: locationBody});
-    zip = jsonQuery('value[types=postal_code].short_name', {data: zip}).value;
-    request('http://api.developer.sears.com/v2.1/products/search/Sears/json/keyword/' + query + '?apikey=' + secrets.searsKey, function(error, resp, body){
-      if (error) {
-        res.send(error);
-      } else {
-        searchResults = JSON.parse(body);
-          products = searchResults.SearchResults.Products
-          new Promise(function(){
-            Promise.each(products,function(item, idx){
-              console.log(idx);
-              var partNumber = item.Id.PartNumber.replace(/P/, '')
-              getLocalProduct(partNumber, zip, function(result){
-                var responses = 0,
-                    temp = [];
-                  if (result){
-                    console.log('result  \n\n\n\n', result, 'end result \n\n\n');
-                    console.log('item \n\n', item, 'end item \n\n\n\n');
-                    temp = [result[0], item];
-                    localResults.push(temp);
-                    console.log('\n\n\n local results ',localResults, "\n\n\n\n\n\n\n\n\n\n");
-                  }
-                })
-            })          
-            console.log(localResults, '\n\n MMMMMMMMMMMMM');
-          }).then(function() { console.log(localResults, '\n\n AHHHH'); res.send(localResults)});
+  request("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + latlong + "&sensor=false").spread(function(response, locationBody){
+      locationBody = JSON.parse(locationBody);
+      zip = jsonQuery('results[types=postal_code].address_components', {data: locationBody});
+      zip = jsonQuery('value[types=postal_code].short_name', {data: zip}).value;
+      request('http://api.developer.sears.com/v2.1/products/search/Sears/json/keyword/' + query + '?apikey=' + secrets.searsKey).spread(function(response, searchResults){
+        searchResults = JSON.parse(searchResults);
+        products = searchResults.SearchResults.Products;
+        Promise.map(products, function(item){
+        var partNumber = item.Id.PartNumber.replace(/P/, '');
+          return getLocalProduct(partNumber, zip).then(function(data){
+            return [data, item];
+          });
+        }).then(function(localResults) {
+          return localResults.filter(function(n){ return n[0] != undefined});
+        })
+        .then(function(results) { console.log('testing \n\n\n\n\n\n\n\n\n'); res.send( {data:results})})
+          .catch(function(e){console.log(e);})
 
-        }
-      }
-    )
-  })
+    })
+  })                
 }
